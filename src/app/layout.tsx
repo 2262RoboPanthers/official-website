5f61ac547303b8cac7b80f6aef953863d1a46d3b
user-dot-py <234708864+user-dot-py@users.noreply.github.com>
2026-08-31 22:55:29 -0400

    finally fixed the flipping scroll bar!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={raleway.className}>
-      <body>
+      <body style={{ overflowX: "hidden" }}>
         <Navbar />
         {children}
         <Footer />
